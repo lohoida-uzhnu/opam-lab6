02a2afa5c7617fb15c1c6605fcfb1d810d58188a
lohoida-uzhnu <c.lohoida.stanislav@student.uzhnu.edu.ua>
2026-02-11 23:26:54 +0200

    create 3 derived classes

--- opam-lab6/Program.cs
+++ opam-lab6/Program.cs
@@ -5,3 +5,28 @@ public abstract class Vehicle
     public double speed;
     public abstract void Move();
 }
+
+
+public class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Машина їде!");
+    }
+}
+
+public class Bicycle : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Велосипед їде!");
+    }
+}
+
+public class Airplane : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Літак летить!");
+    }
+}
